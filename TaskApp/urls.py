f103bded694c34b471274659a119e606726da41f
Akshay-Bhatia22 <dpsvn.aksh06001@gmail.com>
2025-03-25 12:48:31 +0530

    Add get task api

--- TaskApp/urls.py
+++ TaskApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateTaskView, AssignTaskView
+from .views import CreateTaskView, AssignTaskView, GetTask
 
 urlpatterns = [
     path('create', CreateTaskView.as_view(), name='task view'),
-    path('assign/<int:id>', AssignTaskView.as_view(), name='assign task')
+    path('assign/<int:id>', AssignTaskView.as_view(), name='assign task'),
+    path('view/<int:id>', GetTask.as_view(), name='view task')
 
 ]
--- TaskApp/views.py
+++ TaskApp/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView, ListAPIView
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -19,11 +19,24 @@ class AssignTaskView(APIView):
 
     def patch(self, request, id):
         task = Task.objects.get(id=id)
-        user = UserAccount.objects.get(id=request.data.get("user"))
-        print(task)
-        # TODO multiple users 
-        if task and user:
-            task.assignee.add(user)
-            return Response("message", status=status.HTTP_200_OK)
-        return Response("message", status=status.HTTP_200_OK)
+        if not task:
+            return Response("Task not found", status=status.HTTP_400_BAD_REQUEST)
         
+        # check if supplied users exist
+        valid_users = UserAccount.objects.filter(id__in=request.data.get("user"))
+        user_ids = list(valid_users.values_list("id", flat=True))
+
+        if request.data.get("operation") == "remove":
+            task.assignee.remove(*user_ids)
+            return Response(f"Assignee user ids {user_ids} deleted succesfully", status=status.HTTP_200_OK)
+
+        task.assignee.add(*user_ids)
+        return Response(f"Assigned user ids {user_ids} succesfully", status=status.HTTP_200_OK)
+        
+class GetTask(ListAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        assignee_id = self.kwargs["id"]
+        return Task.objects.filter(assignee=assignee_id)
